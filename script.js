ffa2edbdfb54b5d463b4a2805fcd2b9a9fae68d1
Kratika Tomar <tomar.kratika14@gmail.com>
2026-05-01 17:22:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth button click effect
+document.querySelector(".primary").addEventListener("click", () => {
+  alert("🚀 Welcome to YourCosmos!");
+});
+
+// Learn More button click
+document.querySelector(".secondary").addEventListener("click", () => {
+  alert("🌌 More features coming soon!");
+});
